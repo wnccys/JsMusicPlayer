dd88d5cf892696c4154b8127a06384aa11b64db3
Vinicius Souza <ipooo346@gmail.com>
2023-02-22 14:25:31 -0300

    set utils import properly

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 import audios from "./data.js";
-import path from "./utils";
+import { path } from "./utils.js";
 export default {
     cover: document.querySelector(".card-image"),
     title: document.querySelector(".card-content h5"),
