a76d935f9e81cc132e411df59bff26a742d92cb1
Vinicius Souza <ipooo346@gmail.com>
2023-04-08 15:44:29 -0300

    setting up audio options

--- player.js
+++ player.js
@@ -2,7 +2,8 @@ import audios from "data.js";
 
 export default {
     audioData: audios,
+    currentPlaying: 0,
     start(){
-        this.audio.play();    
+        this.audio.play();
     }
 }
---
+++ playerElements.js
@@ -0,0 +1,5 @@
+export default {
+    createAudioElement(audio) {
+        this.audio = new Audio(audio);
+    },
+}
