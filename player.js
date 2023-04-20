aa3eda2eb3fb03265d2ce26f8430759efb2ad9fc
Vinicius Souza <ipooo346@gmail.com>
2023-04-20 11:58:03 -0300

    set play functions and set correctly player objects

--- player.js
+++ player.js
@@ -6,20 +6,36 @@ export default {
     audioData: audios,
     currentAudio: {},
     currentPlaying: 0,
+    isPlaying: false,
 
     start(){
         elements.properties.call(this);
 
         this.update();
     },
-
+    play(){
+        this.audio.play();
+        isPlaying = true;
+    },
+    pause(){
+        this.audio.pause();
+        isPlaying = false;
+    },
+    togglePlayPause(){
+        this.isPlaying ? this.pause() : this.play();
+    },
     update(){
         this.currentAudio = this.audioData[this.currentPlaying];
+
         this.image.style.background = `url('${path(
             this.currentAudio.cover
             )}') no-repeat center center / cover `;
-
+        this.title.innerText = this.currentAudio.title;
+        this.artist.innerText = this.currentAudio.artist;
         elements.createAudioElement.call(this, path(this.currentAudio.file));
 
+        this.audio.onloadeddata = () => {
+            elements.actions.call(this);
+        }
     }
 }
--- playerElements.js
+++ playerElements.js
@@ -1,16 +1,15 @@
 export default {
     properties(){
         this.image = document.querySelector(".img");
-        this.title = document.querySelector(".player-data p");
-        this.artist = document.querySelector(".player-data h4"); 
-        this.seekbar = document.querySelector("#seekbar")
-        this.playPause = document.querySelector(".play_button")
-    }
-    ,
+        this.title = document.querySelector("#music");
+        this.artist = document.querySelector("#artist"); 
+        this.seekbar = document.querySelector("#seekbar");
+        this.playPause = document.querySelector(".play_button");
+    },
     createAudioElement(audio) {
         this.audio = new Audio(audio);
     },
-    actions (){
-        this.playPause.onclick = () => { this.togglePlayPause() };
+    actions(){
+        this.playPause.onclick = this.togglePlayPause();
     }
 }
